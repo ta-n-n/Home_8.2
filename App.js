acc8bb48aa32a639ae3435735362c0ba9e053097
tanapp <tann2682004@gmail.com>
2024-10-09 18:02:13 +0700

    noi dung

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.js
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { PhoneProvider } from '../tanlion/PhoneContext';
+import SignInScreen from '../tanlion/SignInScreen';
+import HomeScreen from '../tanlion/HomeScreen';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PhoneProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="SignIn">
+          <Stack.Screen name="SignIn" component={SignInScreen} options={{ title: 'Đăng nhập' }} />
+          <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Trang chủ' }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PhoneProvider>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ README.md
@@ -0,0 +1,2 @@
+![alt](B8.2.png)
+![alt](Bai8.2.png)
